f9de2f4a51471db305a5a23571f279c99f7e4f2f
ficarra1002 <ficarra1002@gmail.com>
2018-11-27 10:23:54 -0600

    Fully merged with jbuild

---
+++ @BuilderItems/Addons/builder/proxy/config.cpp
@@ -0,0 +1,117 @@
+class CfgPatches
+{
+	class DZ_Structures_jerome
+	{
+		units[]={};
+		weapons[]={};
+		requiredVersion=0.1;
+		requiredAddons[]=
+		{
+			"DZ_Data"
+		};
+	};
+};
+class CfgVehicles
+{
+	class HouseNoDestruct;
+	class bldr_AirCond_big: HouseNoDestruct
+	{
+		scope=1;
+		model="\DZ\structures\Proxy_BuildingParts\aircondition\aircond_big.p3d";
+	};
+	class bldr_AirCond_small: HouseNoDestruct
+	{
+		scope=1;
+		model="\DZ\structures\Proxy_BuildingParts\aircondition\aircond_small.p3d";
+	};
+	class bldr_door_planks_01: HouseNoDestruct
+	{
+		scope=1;
+		model="\DZ\structures\Proxy_BuildingParts\barricading\door_planks_01.p3d";
+	};
+	class bldr_GasMeterExt: HouseNoDestruct
+	{
+		scope=1;
+		model="\DZ\structures\Proxy_BuildingParts\gasmeterext\gasmeterext.p3d";
+	};
+	class bldr_HouseV2_02_window: HouseNoDestruct
+	{
+		scope=1;
+		model="\DZ\structures\Proxy_BuildingParts\house\housev2_02_window.p3d";
+	};
+	class bldr_ladder_long_proxy: HouseNoDestruct
+	{
+		scope=1;
+		model="\DZ\structures\Proxy_BuildingParts\ladders\ladder_long_proxy.p3d";
+	};
+	class bldr_ladder_medium_proxy: HouseNoDestruct
+	{
+		scope=1;
+		model="\DZ\structures\Proxy_BuildingParts\ladders\ladder_medium_proxy.p3d";
+	};
+	class bldr_ladder_short_proxy: HouseNoDestruct
+	{
+		scope=1;
+		model="\DZ\structures\Proxy_BuildingParts\ladders\ladder_short_proxy.p3d";
+	};
+	class bldr_ladder_top_proxy: HouseNoDestruct
+	{
+		scope=1;
+		model="\DZ\structures\Proxy_BuildingParts\ladders\ladder_top_proxy.p3d";
+	};
+	class bldr_LightningConductorBottom: HouseNoDestruct
+	{
+		scope=1;
+		model="\DZ\structures\Proxy_BuildingParts\lightningcond\lightningconductorbottom\lightningconductorbottom.p3d";
+	};
+	class bldr_LightningConductorConnection: HouseNoDestruct
+	{
+		scope=1;
+		model="\DZ\structures\Proxy_BuildingParts\lightningcond\lightningconductorconnection\lightningconductorconnection.p3d";
+	};
+	class bldr_LightningConductorCorner: HouseNoDestruct
+	{
+		scope=1;
+		model="\DZ\structures\Proxy_BuildingParts\lightningcond\lightningconductorcorner\lightningconductorcorner.p3d";
+	};
+	class bldr_LightningConductorMiddle: HouseNoDestruct
+	{
+		scope=1;
+		model="\DZ\structures\Proxy_BuildingParts\lightningcond\lightningconductormiddle\lightningconductormiddle.p3d";
+	};
+	class bldr_LightningConductorMiddleShort: HouseNoDestruct
+	{
+		scope=1;
+		model="\DZ\structures\Proxy_BuildingParts\lightningcond\lightningconductormiddle\lightningconductormiddleshort.p3d";
+	};
+	class bldr_LightningConductorTop: HouseNoDestruct
+	{
+		scope=1;
+		model="\DZ\structures\Proxy_BuildingParts\lightningcond\lightningconductortop\lightningconductortop.p3d";
+	};
+	class bldr_AirCondition_A: HouseNoDestruct
+	{
+		scope=1;
+		model="\DZ\structures\Proxy_BuildingParts\roof\aircondition\aircondition_a.p3d";
+	};
+	class bldr_AirCondition_B: HouseNoDestruct
+	{
+		scope=1;
+		model="\DZ\structures\Proxy_BuildingParts\roof\aircondition\aircondition_b.p3d";
+	};
+	class bldr_antenna_big_roof: HouseNoDestruct
+	{
+		scope=1;
+		model="\DZ\structures\Proxy_BuildingParts\roof\antennabigroof\antenna_big_roof.p3d";
+	};
+	class bldr_antenna_small_roof: HouseNoDestruct
+	{
+		scope=1;
+		model="\DZ\structures\Proxy_BuildingParts\roof\antennasmallroof\antenna_small_roof.p3d";
+	};
+	class bldr_antenna_small_roof_1: HouseNoDestruct
+	{
+		scope=1;
+		model="\DZ\structures\Proxy_BuildingParts\roof\antennasmallroof\antenna_small_roof_1.p3d";
+	};
+};
